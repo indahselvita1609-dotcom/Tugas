bce555f4f7b479ba6c9daa9285ae369e7347966d
indahselvita1609-dotcom <indahselvita1609@gmail.com>
2026-04-18 17:06:47 +0700

    initial commit

---
+++ menyapa.dart
@@ -0,0 +1,18 @@
+void main() {
+  greetings();
+  print(Kabar());
+  var UcapanSyukur = "Alhamdulilah";
+  jawaban(UcapanSyukur);
+}
+
+void greetings() {
+  print("Hai Semua");
+}
+
+String Kabar() {
+  return "Apa Kabar?";
+}
+
+void jawaban(UcapanSyukur) {
+  print("$UcapanSyukur sangat baik");
+}
